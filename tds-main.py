e88544ea88182aea0fd86cffb6c9c341494dd9ad
log <lmhall03@louisville.edu>
2017-01-06 16:31:28 -0500

    refactored code

--- tds-main.py
+++ tds-main.py
@@ -1,18 +1,35 @@
 # -*- coding: utf-8 -*-
 
+# ==============================================================
+#
+# Target Detection System (TDS) code for detecting targets as a 
+# part of NASA student launch. Essentially, color ranges for the 
+# color of each target is defined. A still is taken and this image
+# is masked using each color range (along with some other image 
+# processing techniques). Contours are taken from the mask image
+# and approximated to determine shape. Shapes identified as squares
+# are then verified as legitimate targets (based on altitude, etc). 
+# If a target is detected, an image of the target marked is saved for
+# verification purposes. The object detect function performs these steps;
+# it will return a tuple of booleans based on the targets found (b, r, y). 
+#
+# Much of this code (HSV range, intermediate image processing) 
+# is based on a tutorial found here: 
+# https://gurus.pyimagesearch.com/object-tracking-in-video/
+# 
+# The square detection portion is found here: 
+# http://www.pyimagesearch.com/2016/02/08/opencv-shape-detection/
+#
+# ===============================================================
+
 import imutils
 import sys
 import cv2
 import time
 import copy
 import os.path
-# ==============================================================
+
 # Define color ranges for color recognition... in HSV
-#
-# Color 1(Blue)   –  BGR(91,32,0)      HSV(109.5,255,35.7)      Pantone = 281C
-# Color 2(Yellow) –  BGR(0,209,255)    HSV(24.5,255,255)        Pantone = 109C
-# Color 3(Red)    –  BGR(61,9,166)     HSV(165,94.6,65.1)     Pantone = 1945C
-# ==============================================================
 color_ranges = [
 		((100,50,50),(130,255,255), "b"),
 		((20,100,100),(40,255,255),"y"),
@@ -22,10 +39,10 @@ color_ranges = [
 # Get still from pi cam and return
 # Currently a STUB
 # ==============================================================
-def get_still():
+def getStill():
 	return cv2.imread('test-materials/test-footage-still.JPG') 
 
-def get_candidates(mask):
+def getCandidates(mask):
 	candidates = []
 	
 	# find contours
@@ -46,16 +63,16 @@ def get_candidates(mask):
 				ratio = w / float(h)
 				
 				# assume square if aspect ratio is within 80% - 120%
-				if(ar >= .8 and ar <= 1.2):
+				if(ratio >= .8 and ratio <= 1.2):
 					candidates.append(cnt)
 	
 	return candidates
 
 # stub: need to do physics stuff
-def verify_candidates(candidates):
+def verifyCandidates(candidates):
 	return candidates[0]
 
-def check_targets_found():
+def checkTargetsFound():
 	color_string = 'bry'
 	found = {}
 	
@@ -70,17 +87,17 @@ def check_targets_found():
 			
 	return found
 
-def object_detect():
+def objectDetect():
 	# check which targets already found/set others to false 
-	found = check_targets_found()
+	found = checkTargetsFound()
 	
 	# first, get image to process
-	image = get_still()
+	image = getStill()
 	
 	# resize, blur, and convert to hsv color space
 	image = imutils.resize(image, 1200) 
 	blurred = cv2.GaussianBlur(image, (11,11), 0)
-	hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
 	for (lower, upper, color_name) in color_ranges:
 		# skip if this color already found 
@@ -95,10 +112,10 @@ def object_detect():
 		mask = cv2.dilate(mask, None, iterations = 2) 
 		
 		# get square candidates
-		candidates = get_candidates(mask) 
+		candidates = getCandidates(mask) 
 		
 		# verify which square (if any) is a target
-		target = verify_candidates(candidates) 
+		target = verifyCandidates(candidates) 
 		
 		# mark image and save if target founf
 		if(target is not None):
@@ -119,7 +136,7 @@ def object_detect():
 	return found['b'], found['r'], found['y']
 
 def main():
-	l = object_detect()
+	l = objectDetect()
 	cv2.destroyAllWindows()
 
 if __name__ == "__main__":
